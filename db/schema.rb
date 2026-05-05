e38bebc3fb6f24799da9a0f5c53b5e6de1a71337
Sumit Luitel <sltel@Sumits-MacBook-Pro.local>
2026-05-05 14:39:30 +0545

    Update and Migrate user models

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_04_26_093558) do
+ActiveRecord::Schema[8.1].define(version: 2026_05_05_083550) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
   create_table "users", force: :cascade do |t|
+    t.string "country"
     t.datetime "created_at", null: false
     t.string "email"
+    t.string "first_name"
+    t.string "last_name"
     t.string "password_digest"
     t.datetime "updated_at", null: false
   end
