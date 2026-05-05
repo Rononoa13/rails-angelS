9824d5204ee5cebfc16a57caec269c5e181d61b2
Sumit Luitel <sltel@Sumits-MacBook-Pro.local>
2026-05-05 19:19:28 +0545

    Signin with JWT

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -1,6 +1,8 @@
 class Api::V1::AuthController < ApplicationController
     skip_before_action :verify_authenticity_token
-
+    ###########
+    # Sign Up #
+    ###########
     def signup
         user = User.new(user_params)
         if user.save
@@ -13,6 +15,34 @@ class Api::V1::AuthController < ApplicationController
                 message: "User creation failed",
                 errors: user.errors.full_messages
             }, status: :unprocessable_entity
+        end
+    end
+
+    ###########
+    # Sign In #
+    ###########
+    def signin
+        user = User.find_by(email: params[:auth][:email])
+
+        if user&.authenticate(params[:auth][:password])
+            token = encode_token({ user_id: user.id })
+            
+            render json: {
+                data: {
+                    id: user.id,
+                    type: "users",
+                    attributes: {
+                    token: token,
+                    email: user.email,
+                    name: "#{user.first_name} #{user.last_name}",
+                    country: user.country
+                    }
+                }
+            }, status: :ok
+        else
+            render json: { 
+                message: "Invalid email or password"
+            }, status: :unauthorized
         end
     end
 
